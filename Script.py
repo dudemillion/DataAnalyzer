d95fa97e080e1161355f56ef2b210d0545e817b4
dudemillion <96347362+dudemillion@users.noreply.github.com>
2024-11-13 21:08:37 -0500

    finally

--- Script.py
+++ Script.py
@@ -96,7 +96,7 @@ def parse_arguments():
 def main():
     args = parse_arguments()
     file_path = (
-        r'C:\Users\Nick\Documents\Python Scripts\Competition Folder\3842963.csv'
+        r'C:\Users\Nick\Documents\Python Scripts\TaskFolder\3842963.csv'
         )
     data = load_data(file_path)
     update_tavgdict(data)
